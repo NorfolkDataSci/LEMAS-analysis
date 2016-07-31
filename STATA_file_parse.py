5307d8ad91c4bd120d2c7eacbf0798bc7f146478
Josiah Baker <josiah.baker@forrent.com>
2016-07-31 16:04:25 -0400

    removed .tsv files, added script for parsing ascii fwf's. left STATA .dct files in for reference

--- STATA_file_parse.py
+++ STATA_file_parse.py
@@ -1,15 +1,32 @@
+"""
+    Author: Josiah Baker
+    Org: Data Science User Group
+
+    Simple script for reading a ASCII.txt file with a STATA .dct file into a CSV
+    To use: 
+        
+        python STAT_file_parse.py <asciifile> <setupfile>
+"""
+
 import re
 import pandas as pd
-        
+from sys import argv
+
 digits = re.compile('\%([0-9]?[0-9])')
 col_name = re.compile('"(.*?)"')
 
-with open('/users/josiah.baker/downloads/ICPSR_03079 3/DS0001/03079-0001-Setup.dct') as data:
-    col_width = [int(digits.findall(line)[0]) for line in data.readlines() if digits.findall(line)]
-    name = re.compile('"(.*?)"')
-    data.seek(0)
-    col_names = [name.findall(line)[0] for line in data.readlines() if digits.findall(line)]
-    
-df = pd.read_fwf('/users/josiah.baker/downloads/ICPSR_03079 3/DS0001/03079-0001-Data.txt', widths=col_width, header=None, names=col_names)
+def main(txtfile, setupfile):
+    try:
+        digits = re.compile('\%([0-9]?[0-9])')
+        name = re.compile('"(.*?)"')
+        with open(setupfile) as data:
+            col_width = [int(digits.findall(line)[0]) for line in data.readlines() if digits.findall(line)]
+            data.seek(0)
+            col_names = [name.findall(line)[0] for line in data.readlines() if digits.findall(line)]
+            df = pd.read_fwf(txtfile, widths=col_width, header=None, names=col_names)
+            df.to_csv(txtfile.split('.')[0] + '_with_headers.csv', index=False)
+    except:
+        print "There was a problem parsing " + argv[1] 
 
-print df.head()
+if __name__ == "__main__":
+    main(argv[1], argv[2])
